49e9b5daa28f47dcf06b71a8ac3cf61ecd2bd65a
Whalerus <xxdaoverlordxx@gmail.com>
2015-11-03 19:15:56 -0600

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test extends Thread{
-  public static void main (String [] args){
-    System.out.println(this.toByteArray());
-  }
-  
-  public static void run(){
-    super.run();
-  }
-}
